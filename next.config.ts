157f6da0bda942a765ca6bfd7eda5ca1164b69c0
Elliott Liu <16389800+elliott-liu@users.noreply.github.com>
2026-01-16 22:18:53 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "export",
-
-	basePath: process.env.NEXT_PUBLIC_BASE_PATH || undefined,
-
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
 	images: {
 		unoptimized: true,
 	},
