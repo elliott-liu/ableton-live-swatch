de14803d8ef638976427818128c59dfa7c63b68f
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2026-01-20 20:37:04 +0800

    Add suspense boundary to Swatch

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import { Suspense } from "react";
+
+import { LoadingSpinner } from "@/components/LoadingSpinner";
 import { Swatch } from "@/components/Swatch";
 
 export default function Page() {
 	return (
-		<main>
-			<Swatch />
+		<main className="h-full w-full">
+			<Suspense
+				fallback={
+					<div className="my-12 flex h-full w-full items-center justify-center py-12">
+						<LoadingSpinner className="size-6" />
+					</div>
+				}
+			>
+				<Swatch />
+			</Suspense>
 		</main>
 	);
 }
